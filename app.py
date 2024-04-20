464c50beb334f5b78723adf6b94f62984f89e27f
Piero Delgado <112528610+PieroD04@users.noreply.github.com>
2024-04-20 17:43:26 -0500

    feat: added connection with mysql

--- app.py
+++ app.py
@@ -10,23 +10,23 @@ app = Flask(
 
 
 # Connect to MySQL database
-# db_connection = mysql.connector.connect(
-#     host="db-libros.mysql.database.azure.com",
-#     user="Admin123",
-#     password="321nimdA",
-#     database="db_bookshop"  # Change to your database name
-# )
-# cursor = db_connection.cursor()
+db_connection = mysql.connector.connect(
+    host="db-libros.mysql.database.azure.com",
+    user="Admin123",
+    password="321nimdA",
+    database="db_libros"  # Change to your database name
+)
+cursor = db_connection.cursor()
 
 # Route to display books
 @app.route('/')
-def index():
-    return render_template('index.html')    
-# def show_books():
-#     Query database to get book information
-#     cursor.execute("SELECT * FROM libros")
-#     books = cursor.fetchall()
-#     return render_template('index.html', books=books)
+# def index():
+#     return render_template('index.html')    
+def show_books():
+    #Query database to get book information
+    cursor.execute("SELECT * FROM libros")
+    books = cursor.fetchall()
+    return render_template('index.html', books=books)
 
 if __name__ == '__main__':
     app.run(debug=True)
